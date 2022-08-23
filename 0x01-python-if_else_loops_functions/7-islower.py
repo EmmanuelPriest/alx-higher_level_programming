c70974dd4027f298532f707898c6424ed902a5f2
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2022-08-23 14:21:56 -0700

    function that checks for lowercase character

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    for c in range(97, 123):
-        if c >= 'a' and c < 'z':
-            return True
-        else:
-            return False
+    if c >= 'a' and c < 'z':
+        return True
+    else:
+        return False
