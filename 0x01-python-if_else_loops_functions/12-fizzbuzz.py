956f2d7eb1718fa77be7de0bdb442fd8c9bdff42
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2022-08-23 17:09:45 -0700

    function that inserts and prints Buzz, Fizz, FIzzBuzz in the numbers from 1 to 100 separated by a space

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for FizzBuzz in range(101):
-        if FizzBuzz % 3 == 0 and FizzBuzz % 5 == 0:
+    for n in range(101):
+        if n % 3 == 0 and n % 5 == 0:
             print("FizzBuzz")
             continue
-        elif FizzBuzz % 3 == 0:
+        elif n % 3 == 0:
             print("Fizz")
             continue
-        elif FizzBuzz % 5 == 0:
+        elif n % 5 == 0:
             print("Buzz")
             continue
         else:
-            print(end=" ")
+            print("{} $".format(n), end=" ")
