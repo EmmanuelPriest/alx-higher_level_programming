dcd890d06ab7bd6af19901b755a730790c455f9f
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2022-08-25 09:50:38 -0700

    program that imports all functions from the file calculator_1.py and handles basic operations

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -9,12 +9,11 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    operat = {"+": add, "_": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(operat.keys()):
+    ope = {"+": add, "_": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(ope.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
     a = int(sys.argv[1])
     b = int(sys.srgv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b,
-                                 operat[sys.argv[2]](a, b)))
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ope[sys.argv[2]](a, b)))
