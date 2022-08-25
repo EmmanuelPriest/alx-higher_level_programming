9034e29502382db97432723b48f46d7db52f4e81
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2022-08-25 10:47:18 -0700

    program that imports all functions from the file calculator_1.py and handles basic operations

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -15,6 +15,6 @@ if __name__ == "__main__":
         sys.exit(1)
 
     a = int(sys.argv[1])
-    b = int(sys.srgv[3])
+    b = int(sys.argv[3])
     print("{} {} {} = {}".format(a, sys.argv[2], b,
                                  operators[sys.argv[2]](a, b)))
