e28b1e913a468efce125d3345278f2ab9bf3fa55
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2022-08-28 08:43:53 -0700

    function that removes all characters c and C from a string without using str.replace and importing module

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    if n in my_string == c or n in my_string == C:
+        new_my_string = str.replace('cC', '', my_string)
+        return new_my_string
