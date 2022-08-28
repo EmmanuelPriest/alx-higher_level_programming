b155df8c521455cff9c3be0d13e11a6dfea707c8
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2022-08-28 12:04:56 -0700

    function that adds 2 tuples with alot of complexity in the question

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,6 +4,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
     new_tuple = ()
     f_tuple = tuple_a + (0, 0)
     sec_tuple = tuple_b + (0, 0)
-    new_tuple = f_tuple[0] + sec_tuple[0],
-    f_tuple[1] + sec_tuple[1]
+    new_tuple = f_tuple[0] + sec_tuple[0], f_tuple[1] + sec_tuple[1]
     return new_tuple
