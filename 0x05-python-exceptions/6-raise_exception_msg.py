b6affec52324dbb1dd26620bce74136cb445c38d
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2022-09-12 15:06:57 -0700

    function that raises a name exception with a message

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 
 def raise_exception_msg(message=""):
-    try:
-        raise NameError("wrong name")
-    except NameError:
-        print("Exception is raised")
+    raise NameError(message)
