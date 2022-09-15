0250fa2da4fda3b4eb7736284df1f72e2d0dc418
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2022-09-15 13:33:00 -0700

    Write a class Square that defines a square by: (based on 3-square.py)

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,44 +1,48 @@
 #!/usr/bin/python3
-""" class Square that defines a square"""
+
+'''class Square that defines a square'''
 
 
 class Square:
-    """ class Square that defines a square"""
+    '''Creating the init method of class Square'''
+
     def __init__(self, size=0):
-        """ init square
+        '''Init method to initialize square
 
         Args:
-            value (int): size of the square.
-        """
+            size (int): size of the square
+
+        '''
         self.size = size
 
     @property
     def size(self):
-        """int: private size.
+        '''int: private attribute size
 
         Returns:
-            Private size.
-        """
+            Private attribute size
+        '''
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Sets value into size, must be int.
+        '''Setter method that sets value into size, must be int
 
         Args:
-            value (int): size of the square.
-        """
-        if type(value) is not int:
-            raise TypeError('size must be an integer')
+            value (int): value(=size) of the square
+        '''
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
         elif value < 0:
-            raise ValueError('size must be >= 0')
+            raise ValueError("size must be >= 0")
         else:
-            self.__size = value  #: size of the square
+            self.__size = value  #: value(=size) of the square
 
     def area(self):
-        """returns the area
+        '''returns the current square area
 
         Returns:
-            area.
-        """
-        return self.__size**2
+            Area square
+
+        '''
+        return (self.__size ** 2)
