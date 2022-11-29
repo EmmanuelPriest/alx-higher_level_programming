2248bd1278d9787819d87516385372d9afa9bd70
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2022-11-28 19:06:47 -0800

    Update this script to replace the value 12 with 89

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -6,7 +6,5 @@ const myObject = {
 };
 console.log(myObject);
 
-/* const elementIndex = myObject.findIndex((obj => obj.type === 'object')); */
-/* myObject[elementIndex].value = 89; */
-myObject['value'] = 89;
+myObject.value = 89;
 console.log(myObject);
