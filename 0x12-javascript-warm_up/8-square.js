71bc955e764e8780118bbc643910c3acd0409b4c
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2022-11-28 17:20:34 -0800

    script that prints a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const x = process.argv[2];
+if (!parseInt(x)) {
+  console.log('Missing size');
+} else {
+  for (let n = 0; n < x; n++) {
+    /* let m = 0; */
+    /* let mySquare = ''; */
+    for (let m = 0; m < x; m++) {
+      let mySquare += 'X';
+    }
+    console.log(mySquare);
+  }
+}
