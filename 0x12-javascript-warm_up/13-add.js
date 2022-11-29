c08b31221840460bad7f01f8f488347893cb41ae
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2022-11-28 19:28:18 -0800

    function that returns the addition of 2 integers

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,7 +1,3 @@
 #!/usr/bin/node
 
-const x = process.argv[2];
-const y = process.argv[3];
-const func = function add(x, y) {
-  return (parseInt(x) + parseInt(y));
-}
+exports.add = (x, y) => x + y;
