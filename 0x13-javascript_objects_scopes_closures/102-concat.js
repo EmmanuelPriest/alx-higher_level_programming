0754d8992fc3e34138ca6f6eea7f53012e1348ec
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2022-11-30 04:48:18 -0800

    script that concats 2 files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,24 +2,24 @@
 
 /* script that concatenates two files */
 
-const fileStream = require('fileStream');
+const fs = require('fs');
 
 const fileA = process.argv[2];
 const fileB = process.argv[3];
 const fileC = process.argv[4];
 
 if (
-  fileStream.existsSync(fileA) &&
-fileStream.statSync(fileA).isFile &&
-fileStream.existsSync(fileB) &&
-fileStream.statSync(fileB).isFile &&
+  fs.existsSync(fileA) &&
+fs.statSync(fileA).isFile &&
+fs.existsSync(fileB) &&
+fs.statSync(fileB).isFile &&
 fileC !== undefined
 ) {
-   const fileAContent = fileStream.readFileSync(fileA);
-   const fileBContent = fileStream.readFileSync(fileB);
-   const flow = fileStream.createWriteStream(fileC);
+   const fileAContent = fs.readFileSync(fileA);
+   const fileBContent = fs.readFileSync(fileB);
+   const stream = fs.createWriteStream(fileC);
 
-   flow.write(fileAContent);
-   flow.write(fileBContent);
-   flow.end();
+   stream.write(fileAContent);
+   stream.write(fileBContent);
+   stream.end();
 }
