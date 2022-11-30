8e6f927c324b91db06bde4dfde10dce87008e4a2
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2022-11-29 19:55:35 -0800

    function that converts a number from base 10 to another base passed as argument

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -2,5 +2,8 @@
 
 /* convert from base 10 to other bases */
 exports.converter = function (base) {
-  console.log(decimal.toString(base));
+  function myConverter (x) {
+    return x.toString(base);
+  }
+  return myConverter;
 };
