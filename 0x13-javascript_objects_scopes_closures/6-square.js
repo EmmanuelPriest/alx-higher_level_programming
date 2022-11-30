a98c64648ca23f826c28838b13710d0157d816dd
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2022-11-29 17:03:37 -0800

    class Square that defines a square and inherits from Square of 5-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -5,12 +5,12 @@ const FormerSquare = require('./5-square');
 class Square extends FormerSquare {
   charPrint (c) {
     const squareVar = c === undefined ? 'X' : c;
-      for (let n = 0; n < this.height; n++) {
-        let rectVar = '';
-	for (let m = 0; m < this.width; m++) {
-	  rectVar += squareVar;
-	}
-	console.log(rectVar);
+    for (let n = 0; n < this.height; n++) {
+      let rectVar = '';
+      for (let m = 0; m < this.width; m++) {
+        rectVar += squareVar;
+      }
+      console.log(rectVar);
     }
   }
 }
