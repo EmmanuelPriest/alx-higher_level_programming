84a850eb03d322e52f78b972e05b57b3ee6d2de9
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2022-12-03 13:45:40 -0800

    script that prints all City objects from the database hbtn_0e_14_usa

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,8 +1,9 @@
- #!/usr/bin/python3
+#!/usr/bin/python3
 
-'''script that lists all State objects from the database hbtn_0e_6_usa'''
+'''script that prints all City objects from the database hbtn_0e_6_usa'''
 import sys
 from model_state import Base, State
+from model_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -14,7 +15,8 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=createEngine)
     create_sess = Session()
 
-    cities = create_sess.query(City).order_by(City.id).all()
+    state_city = create_sess.query(State, City). \
+        filter(State.id == City.state_id).all()
 
-    for city in cities:
+    for state, city in state_city:
         print("{}: ({}) {}".format(state.name, city.id, city.name))
