ca89a6b5fc8cbfdd61197ce4daf7938fb5daf231
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2022-12-03 07:26:21 -0800

    script that prints the State object with the name passed as argument from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     createEngine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
                                  format(sys.argv[1], sys.argv[2],
                                         sys.argv[3]),
-                                        pool_pre_ping=True)
+                                 pool_pre_ping=True)
     Session = sessionmaker(bind=createEngine)
     create_sess = Session()
 
