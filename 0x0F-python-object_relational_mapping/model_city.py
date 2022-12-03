3e42a8edce0b6747c96d0e791ef887da91ee9b45
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2022-12-03 13:12:30 -0800

    Python file that contains the class definition of a City.
    which inherits from class Base

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+'''
+contains the class definition of a City and class instance
+attributes id and name that represent columns of auto generated
+unique integer and a string of 128 characters repectively
+'''
+from model_state import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+
+
+class City(Base):
+    '''Defines class City that inherits from class Base'''
+    __tablename__ = "cities"
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
