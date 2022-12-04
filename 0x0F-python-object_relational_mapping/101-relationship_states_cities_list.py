87cae32de742f145d1ec2f8f7d8553b479dedf24
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2022-12-04 02:39:07 -0800

    script that lists all State objects, and corresponding City objects, contained in the database hbtn_0e_101_usa

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,29 +1,24 @@
-#!/usr/bin/pyhton3
-
-'''
-script that lists all State objects, and corresponding City objects,
-contained in the database hbtn_0e_101_usa
-'''
-import sys
-from relationship_state import Base, State
-from relationship_city import City
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-from sqlalchemy.schema import Table
-
+#!/usr/bin/python3
+"""lists all State objects, and corresponding City objects,
+contained in the database hbtn_0e_101_usa"""
 
 if __name__ == "__main__":
-    createEngine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
-                                 format(sys.argv[1], sys.argv[2],
-                                        sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(createEngine)
 
-    create_sess = Session(createEngine)
+    import sys
+    from relationship_state import Base, State
+    from relationship_city import City
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    from sqlalchemy.schema import Table
 
-    state_city = create_sess.query(State).order_by(State.id).all()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
-    for state in state_city:
+    session = Session(engine)
+    for state in session.query(State).order_by(State.id).all():
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
             print("    {}: {}".format(city.id, city.name))
-    create_sess.close()
+    session.close()
