8a1cc3b0ecd7011780f012093f2a327d2a479d43
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2022-12-18 05:30:45 -0800

    Python script that fetches 'https://alx-intranet.hbtn.io/status'

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 '''Python script that fetches https://alx-intranet.hbtn.io/status'''
-import urllib
+import urllib.request
 
 
 if __name__ == "__main__":
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
-        req = resp.read()
+        req = res.read()
+        print(req, end="")
