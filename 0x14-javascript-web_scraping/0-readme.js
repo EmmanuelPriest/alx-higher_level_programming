ed51255071b7c63476be4f51c733fa989d687b4e
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2023-01-17 08:55:18 -0800

    Javascript that reads and prints the content of a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,12 +4,12 @@ Script that reads and prints the content of a file
 */
 
 const fs = require('fs');
-const path = require('path');
+// const path = require('path');
 
 const filePath = process.argv[2];
 const encoding = 'utf-8';
 
-fs.readFile(path.resolve(filePath), encoding, (err, data) => {
+fs.readFile(filePath, encoding, (err, data) => {
   if (err) {
     return console.error(err);
   }
