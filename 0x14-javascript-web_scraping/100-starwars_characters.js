969c45c5846883b514584fa7ecee4c258fa6190c
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2023-01-17 16:28:49 -0800

    error

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -8,14 +8,14 @@ const movieId = process.argv[2];
 //const starWarsApiUrl = `https://swapi.dev/api/films/${movieId}/`;
 const starWarsApiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
-request.get(starWarsApiUrl, function (error, res, body) {
+request.get(starWarsApiUrl, (error, res, body) => {
   if (error) {
     console.log(error);
   } else {
     const starWars = JSON.parse(body);
     const characters = starWars.characters;
-    characters.forEach(function (character) {
-      request(character, function (error, res, body) {
+    characters.forEach((character) {
+      request(character, (error, res, body) => {
         if (error) {
           console.log(error);
         } else {
