b30b7a67796d8b7507c6fd0f5c0e19817ebe7ec9
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2023-01-18 03:29:58 -0800

    Script that computes the number of tasks completed by user id

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,16 +3,21 @@
 Script that computes the number of tasks completed by user id
 */
 
+// Importing the request module
 const request = require('request');
+
+// Creating the first user API URL argument based on index
 const userApiUrl = process.argv[2];
 
+// Creating empty key-value objects that will hold the completed tasks
 const userCompletedTask = {};
 
+// Using the request module to get user API URL
 request.get(userApiUrl, (error, res, body) => {
   if (error) {
     return console.error(error);
   }
-  const tasks = JSON.parse(body);
+  const tasks = JSON.parse(body); // Creating JSON file
   tasks.forEach((task) => {
     if (task.completed) {
       if (!userCompletedTask[task.userId]) {
@@ -21,5 +26,5 @@ request.get(userApiUrl, (error, res, body) => {
       userCompletedTask[task.userId] += 1;
     }
   });
-  console.log(userCompletedTask);
+  console.log(userCompletedTask); // Displaying the task completed by the user
 });
