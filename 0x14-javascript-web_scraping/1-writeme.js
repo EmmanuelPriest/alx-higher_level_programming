de454ac0a46290496237490fb9d364f7f3638c1f
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2023-01-18 02:58:15 -0800

    Script that writes a string to a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,14 +2,18 @@
 /*
 Script that writes a string to a file
 */
+
+// Importing the fs module for reading from and writing to a file
 const fs = require('fs');
 const encoding = 'utf-8';
 
+// Creating the first and second arguments based on index
 const filePath = process.argv[2];
 const stringToFile = process.argv[3];
 
+// Using the fs module to write to the file
 fs.writeFile(filePath, stringToFile, encoding, (err) => {
   if (err) {
-    return console.error(err);
+    return console.error(err); // Printing the error object
   }
 });
