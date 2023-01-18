895af19ab5286ddaeeae8a8255e7c6753425292c
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2023-01-17 18:23:13 -0800

    Script that gets the contents of a webpage and stores it in a file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -7,17 +7,16 @@ const request = require('request');
 const fs = require('fs');
 const requestUrl = process.argv[2];
 const filePath = process.argv[3];
-const encoding = 'utf8';
+// const encoding = 'utf8';
 
 request(requestUrl, (error, res, body) => {
   if (error) {
     console.log(error);
   } else {
-    fs.writeFile(filePath, body, encoding, (writeError) => {
+    fs.writeFile(filePath, body, 'utf8', (writeError) => {
       if (writeError) {
         console.log(writeError);
       }
-      console.log(`${filePath}`);
     });
   }
 });
