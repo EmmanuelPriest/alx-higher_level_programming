6c6942139c4278a811d3b2335d82598f09323032
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2023-01-17 17:05:23 -0800

    Script that prints all characters of a Star Wars movie

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -4,25 +4,25 @@ Script that prints all characters of a Star Wars movie
 */
 
 const request = require('request');
+
 const movieId = process.argv[2];
-//const starWarsApiUrl = `https://swapi.dev/api/films/${movieId}/`;
-const starWarsApiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+const starWarsApiUrl = `https://swapi.dev/api/films/${movieId}/`;
+//const starWarsApiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
 request.get(starWarsApiUrl, (error, res, body) => {
   if (error) {
-    console.log(error);
-  } else {
-    const starWars = JSON.parse(body);
-    const characters = starWars.characters;
-    characters.forEach((character) {
-      request(character, (error, res, body) => {
-        if (error) {
-          console.log(error);
-        } else {
-          const charStarWars = JSON.parse(body);
-          console.log(charStarWars.name);
-        }
-      });
+    return console.log(error);
+  }
+
+  const starWars = JSON.parse(body);
+  const characters = starWars.characters;
+  for (const character of characters) {
+    request(character, (error, res, body) => {
+      if (error) {
+        return console.log(error);
+      }
+      const charStarWars = JSON.parse(body);
+      console.log(charStarWars.name);
     });
   }
 });
