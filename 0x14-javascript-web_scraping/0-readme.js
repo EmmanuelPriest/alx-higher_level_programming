983c1c3226e6e62fad864af2321f682acdc55413
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2023-01-18 02:52:58 -0800

    Script that reads and prints the content of a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,15 +3,17 @@
 Script that reads and prints the content of a file
 */
 
+// Importing the fs module for reading from and writing to file
 const fs = require('fs');
-// const path = require('path');
 
+// Creating the first argument based in index
 const filePath = process.argv[2];
 const encoding = 'utf-8';
 
+// Using the fs module to read the file
 fs.readFile(filePath, encoding, (err, data) => {
   if (err) {
     return console.error(err);
   }
-  console.log(data);
+  console.log(data); // printing the error object
 });
